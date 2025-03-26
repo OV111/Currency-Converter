86ae9cea985701344ddfa9fa3fd833672cbd6970
Vahe <ohanyanv703@gmail.com>
2025-03-26 22:32:49 +0400

    api of currency js

---
+++ script.js
@@ -0,0 +1,8 @@
+let key = "cur_live_1bR6N58mlpIgRYSM823thcXmeNNt3s1ADQGyc5DV";
+
+async function api() {
+    let x = await fetch("https://api.currencyapi.com/v3/latest?apikey=cur_live_1bR6N58mlpIgRYSM823thcXmeNNt3s1ADQGyc5DV");
+    console.log(x)
+}  
+api();  
+
