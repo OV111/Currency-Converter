f914d4505f9e41f7f1b0e50a1d03a66c8abfe4f3
Vahe <ohanyanv703@gmail.com>
2025-03-27 20:21:37 +0400

    variables

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let key = "cur_live_1bR6N58mlpIgRYSM823thcXmeNNt3s1ADQGyc5DV";
+const amount = document.getElementById("amount");
+const from = document.getElementById("from");
+const changeBtn = document.getElementById("change");
+const to = document.getElementById("to");
+const convertBtn = document.getElementById("convert");
+
+
+let apiKey = "cur_live_1bR6N58mlpIgRYSM823thcXmeNNt3s1ADQGyc5DV";
 
 async function api() {
     let x = await fetch("https://api.currencyapi.com/v3/latest?apikey=cur_live_1bR6N58mlpIgRYSM823thcXmeNNt3s1ADQGyc5DV");
