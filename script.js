16d8d230d35dc3ed18e5604494350e930bac2fc8
Vahe <ohanyanv703@gmail.com>
2025-03-28 22:01:37 +0400

    change btn

--- script.js
+++ script.js
@@ -15,19 +15,9 @@ async function api() {
         const response = await fetch(`https://v6.exchangerate-api.com/v6/4e6297c2a1106ed9038cce83/latest/USD`);
         if(response.status === 200) {
             const data = await response.json();
-            // console.log(data);
+            console.log(data)
             const rateEntries = Object.entries(data.conversion_rates);        
-            rateEntries.forEach((key) => {
-                const optionFrom = document.createElement("option");
-                optionFrom.text = key[0];
-                optionFrom.value = key[1];
-                from.appendChild(optionFrom);
-    
-                const optionTo = document.createElement("option");
-                optionTo.text = key[0];
-                optionTo.value = key[1];
-                to.appendChild(optionTo);
-            });
+            populateCurrency(rateEntries);
         } else if(response.status === 400) {
             throw new Error("The exchange rates data could not be found (404).");
         } else if(response.status === 500) {
@@ -38,6 +28,24 @@ async function api() {
     } catch(error) {
         console.error("Error fetching data!",error);
     }
-}  
+}
+
+const populateCurrency = (rateEntries) => {
+    rateEntries.forEach((key) => {
+        const optionFrom = document.createElement("option");
+        const optionTo = document.createElement("option");
+        
+        optionFrom.text = key[0];
+        optionFrom.value = key[1];
+        from.appendChild(optionFrom);
+        optionTo.text = key[0];
+        optionTo.value = key[1];
+        to.appendChild(optionTo);
+    });
+}
+changeBtn.addEventListener("click", () => {
+    [from.value,to.value] = [to.value,from.value];
+})
+
 api();
 
