44cbd26510b44ecd3a08ebdd6d225e7de8c7d0a2
Vahe <ohanyanv703@gmail.com>
2025-03-28 21:02:31 +0400

    js new api

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// import { fetchCurrencyData } from "./currscript.js";
 const amount = document.getElementById("amount");
-const from = document.getElementById("from");
+let from = document.getElementById("from");
 const changeBtn = document.getElementById("change");
 const to = document.getElementById("to");
 const rateValue = document.getElementById("rate-value");
@@ -7,11 +8,22 @@ const rateText = document.getElementById("rate-text");
 const convertBtn = document.getElementById("convert");
 
 
-let apiKey = "cur_live_1bR6N58mlpIgRYSM823thcXmeNNt3s1ADQGyc5DV";
+let apiKey = "4e6297c2a1106ed9038cce83";
 
 async function api() {
-    let x = await fetch("https://api.currencyapi.com/v3/latest?apikey=cur_live_1bR6N58mlpIgRYSM823thcXmeNNt3s1ADQGyc5DV");
-    console.log(x)
+    try {
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/4e6297c2a1106ed9038cce83/latest/USD`);
+        const data = await response.json();
+        console.log(data);
+        const rateEntries = Object.entries(data.conversion_rates);        
+        rateEntries.forEach((key) => {
+            const option = document.createElement("option");
+            option.text = key[0];
+            option.value = key[1];
+            from.appendChild(option);
+        });
+    } catch(error) {
+        console.log("Error fetching data!",error);
+    }
 }  
-api();  
-
+api()
